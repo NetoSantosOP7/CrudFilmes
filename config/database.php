fc180d4e06a6889d137251618de08bf47f1c677f
NetoSantosOP7 <148675257+NetoSantosOP7@users.noreply.github.com>
2026-03-24 08:13:41 -0300

    adicionando o port do aiven

--- config/database.php
+++ config/database.php
@@ -2,11 +2,12 @@
 
 $host = $_ENV['DB_HOST'];
 $dbname = $_ENV['DB_NAME'];
+$port = $_ENV['DB_PORT'];
 $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass, [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false]);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;port=$port", $user, $pass, [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false]);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
     
