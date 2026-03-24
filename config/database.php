24a18322a6f65527b705e0a14a5886f51b5af610
NetoSantosOP7 <148675257+NetoSantosOP7@users.noreply.github.com>
2026-03-24 07:32:51 -0300

    fix da ssl do aiven

--- config/database.php
+++ config/database.php
@@ -6,9 +6,10 @@ $user = $_ENV['DB_USER'];
 $pass = $_ENV['DB_PASS'];
 
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass, [PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false]);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    
 } catch (PDOException $e) {
     die("Erro na conexão: " . $e->getMessage());
 }
